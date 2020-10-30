fb7af8ca1df78872dfea401f9ffaabb86a3fb157
ShadyGotRabies <danielspasov4@gmail.com>
2020-10-30 12:06:28 +0200

    Uploading the Rest Services and AJAX lab.

--- JS-Advanced/JS Applications/Rest Services And AJAX/05.Github-Repos/scripts/app.js
+++ JS-Advanced/JS Applications/Rest Services And AJAX/05.Github-Repos/scripts/app.js
@@ -6,14 +6,21 @@ function loadRepos() {
 
 	httpRequest.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
-			console.log(JSON.parse(this.responseText)[0])
+
+			let ul = document.getElementById('repos')
+			let h1 = document.createElement('h1')
+			ul.appendChild(h1)
+
 			JSON.parse(this.responseText).forEach(repo => {
 				let li = document.createElement('li')
 				let a = document.createElement('a')
-				a.href = `https://github.com/${user}/${repo.name}`
+				
+				a.href = repo.html_url
 				a.textContent = repo.full_name
+				h1.textContent = repo.owner.login
+
 				li.appendChild(a)
-				document.getElementById('repos').appendChild(li)
+				ul.appendChild(li)
 			})
 		}
 	}
